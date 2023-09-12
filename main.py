672599e286014084263a31bf3f9d5e9c91013c24
Kanatov Vladislav <99506862+lon8@users.noreply.github.com>
2023-09-12 12:29:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def get_application() -> FastAPI:
 app = get_application()
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000, log_level='info')
+    uvicorn.run(app, host='127.0.0.1', port=8080,log_level='info')
